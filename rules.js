a8bc17a161d1debc51b518174975d404f753aaa2
thomas.carli <thomas.carli@goeuro.com>
2017-02-08 10:15:32 +0100

    change rules

--- rules.js
+++ rules.js
@@ -96,7 +96,7 @@ var rules = [
             this.services.partners.generateComissionPayment(
               this.order.referer_partner,
               this.order.total,
-              0.05)
+              0.10)
             );
             R.next();
           }
